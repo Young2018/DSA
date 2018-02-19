07ceb27875da2f8881c7721eaaf86d49d7c1e2a7
young2018 <young6918@163.com>
2018-02-19 16:38:04 +0800

    LinkedBag类代码框架

--- src/cn/young22/dsa/ch03/LinkedBag.java
+++ src/cn/young22/dsa/ch03/LinkedBag.java
@@ -2,24 +2,30 @@ package cn.young22.dsa.ch03;
 
 import cn.young22.dsa.ch01.BagInterface;
 
-
+/** 包的链式实现的代码框架
+ * 	仅实现了add()和toArray()方法
+ * */
 
 public class LinkedBag<T> implements BagInterface<T>{
+	/** 包中的节点类*/
 	private class Node {
 		private T 	data;	// Entry in bag
 		private Node next; 	// Link to the next node
 		
+		/** 节点的构造方法，将客户传入的dataPortion赋值给节点，next赋值为null*/
 		private Node(T dataPortion){
 			this(dataPortion, null);
 		}
 		
+		/** 节点的构造方法，将客户端传入的dataPortion和nextNode分别赋值给data和next*/
 		private Node(T dataPortion, Node nextNode){
 			data = dataPortion;
 			next = nextNode;
 		}
 	}
-	
+	//头结点
 	private Node firstNode;
+	//包中的节点个数
 	private int numberOfEntries;
 	
 	
@@ -42,7 +48,6 @@ public class LinkedBag<T> implements BagInterface<T>{
 		newNode.next = firstNode;	// make new node reference rest of chain
 									// (firstNode is null if chain is empty)
 									// New node is at beginning of chain
-		
 		firstNode = newNode;
 		numberOfEntries++;
 		
