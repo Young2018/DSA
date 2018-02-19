982ae6d129fda84fcad20eacd6c97ec2e8846331
young2018 <young6918@163.com>
2018-02-19 23:15:09 +0800

    添加了类的说明和不平衡表达式的测试语句

--- src/cn/young22/dsa/ch05/BalanceCheckerDemo.java
+++ src/cn/young22/dsa/ch05/BalanceCheckerDemo.java
@@ -1,5 +1,8 @@
 package cn.young22.dsa.ch05;
 
+/** 测试BalanceChecker类，
+ *  使用一个平衡的表达式和一个不平衡的表达式来测试BalanceChecker类
+ * */
 public class BalanceCheckerDemo {
 	public static void main(String[] args){
 		String expression = "a {b [c (d + e)/2 - f] + 1}";
@@ -10,7 +13,19 @@ public class BalanceCheckerDemo {
 		}else{
 			System.out.println(expression + " is not balanced");
 		}
+		
+		String expression1 = "a {b [c (d + e)/2 - f + 1}";
+		boolean isBalanced1 = BalanceChecker.checkBalance(expression1);
+		if(isBalanced1){
+			System.out.println(expression1 + " is balanced");
+		}else{
+			System.out.println(expression1 + " is not balanced");
+		}
 	}
 }
-//expected output
-//a {b [c (d + e)/2 - f] + 1} is balanced
+/*
+Output:
+
+a {b [c (d + e)/2 - f] + 1} is balanced
+a {b [c (d + e)/2 - f + 1} is not balanced
+*/
