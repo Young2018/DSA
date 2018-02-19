1446efc583b9af93b2df71aa420903a9906e5b7f
young2018 <young6918@163.com>
2018-02-19 18:49:07 +0800

    在LinkedBagDemo2中新增了一些测试方法

--- src/cn/young22/dsa/ch03/LinkedBagDemo2.java
+++ src/cn/young22/dsa/ch03/LinkedBagDemo2.java
@@ -17,15 +17,24 @@ public class LinkedBagDemo2 {
 		String[] contentsOfBag = {"A", "A", "B", "A", "C", "fadsd", "A", "A", "F", "G"};
 		BagInterface<String> aBag = new LinkedBag2<>();
 		
-		//Adding Strings
+		// Testing method isEmpty()
+		testIsEmpty(aBag, true);
+		
+		// Adding Strings
 		testAdd(aBag, contentsOfBag);
 		
-		//Removing Strings
+		// Removing Strings
 		String[] testString = {"", "B", "A", "C", "D", "A"};
 		testRemove(aBag, testString);
 		
+		// Testing method contains()
+		String[] testContains = {"A", "B", "C", "D", "E"};
+		testContains(aBag, testContains);
 		
-		//test method removeAll(T anEntry)
+		// Testing method getFrequencyOf
+		testFrequency(aBag, testString);
+		
+		// Tests method removeAll(T anEntry)
 		String[] testString1 = {"A"};
 		testRemoveAll(aBag, testString1);
 		
@@ -88,10 +97,57 @@ public class LinkedBagDemo2 {
 		
 		System.out.println();
 	} // end displayBag
+	
+	//Tests the method isEmpty
+	//correctResult indicates what isEmpty should return
+	private static void testIsEmpty(BagInterface<String> aBag, boolean correctResult){
+		System.out.print("Testing the method isEmpty with ");
+		if(correctResult){
+			System.out.println("an empty bag");
+		}else{
+			System.out.println("a bag that is not empty");
+		}
+		
+		System.out.println("isEmpty finds the bag ");
+		if(correctResult && aBag.isEmpty()){
+			System.out.println("empty: OK.");
+		}else if(correctResult){
+			System.out.println("not empty, but it is empty: ERROR");			
+		}else if(!correctResult && aBag.isEmpty()){
+			System.out.println("empty, but it is not empty: ERROR");
+		}else{
+			System.out.println("not empty: OK");
+		}
+		System.out.println();
+	}
+	
+	//Tests the method getFrequency
+	private static void testFrequency(BagInterface<String> aBag, String[] tests){
+		System.out.println("\nTesting the method of getFrequency of:");
+		for(int index = 0; index < tests.length; index++){
+			System.out.println("In this bag, the count of " + tests[index] + 
+					" is " + aBag.getFrequencyOf(tests[index]));
+		}
+		System.out.println();
+	}
+	
+	//Tests the method contains
+	private static void testContains(BagInterface<String> aBag, String[] tests){
+		System.out.println("Testing the method contains:");
+		for (int index = 0; index < tests.length; index++){
+			System.out.println("Does this bag cotain " + tests[index] +
+					"? " + aBag.contains(tests[index]));
+		}
+	}
 }
 
 /*
-output:
+Output:
+
+Testing the method isEmpty with an empty bag
+isEmpty finds the bag 
+empty: OK.
+
 Adding A A B A C fadsd A A F G 
 The bag contains 10 string(s), as follows:
 G F A A fadsd C A B A A 
@@ -125,11 +181,25 @@ Removing "A" from the bag:
 remove("A") returns true
 The bag contains 5 string(s), as follows:
 fadsd A F A A 
+Testing the method contains:
+Does this bag cotain A? true
+Does this bag cotain B? false
+Does this bag cotain C? false
+Does this bag cotain D? false
+Does this bag cotain E? false
+
+Testing the method of getFrequency of:
+In this bag, the count of  is 0
+In this bag, the count of B is 0
+In this bag, the count of A is 3
+In this bag, the count of C is 0
+In this bag, the count of D is 0
+In this bag, the count of A is 3
+
 
 Tests method removeAll()
 removeAll("A") returns true
 The bag contains 2 string(s), as follows:
 fadsd F 
-
 
 */
