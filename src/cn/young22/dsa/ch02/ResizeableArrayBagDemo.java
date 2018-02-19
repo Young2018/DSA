e187912798e9e3f8f517625b262b8be6c8f1f743
young2018 <young6918@163.com>
2018-02-19 15:06:27 +0800

    添加ResizeableArrayBagDemo的说明，部分方法注释及测试结果

--- src/cn/young22/dsa/ch02/ResizeableArrayBagDemo.java
+++ src/cn/young22/dsa/ch02/ResizeableArrayBagDemo.java
@@ -1,7 +1,9 @@
 package cn.young22.dsa.ch02;
 
 import cn.young22.dsa.ch01.BagInterface;
-
+/**
+ *  测试ResizeableArrayBag类
+ * */
 public class ResizeableArrayBagDemo {
 
 	public static void main(String[] args) {
@@ -42,6 +44,7 @@ public class ResizeableArrayBagDemo {
 		displayBag(aBag);
 	}
 	
+	/** Tests the method add*/
 	private static void testAdd(BagInterface<String> aBag, String[] contents){
 		System.out.print("Adding to the bag: ");
 		for(int index = 0; index < contents.length; index++){
@@ -53,7 +56,7 @@ public class ResizeableArrayBagDemo {
 		displayBag(aBag);
 	}//end testAdd
 	
-	//Tests the two remove methods
+	/** Tests the two remove methods*/
 	private static void testRemove(BagInterface<String> aBag, String[] tests){
 		for(int index = 0; index < tests.length; index++){
 			String aString = tests[index];
@@ -72,7 +75,7 @@ public class ResizeableArrayBagDemo {
 		}
 	}
 	
-	//Tests the method isEmpty
+	/** Tests the method isEmpty*/
 	//correctResult indicates what isEmpty should return
 	private static void testIsEmpty(BagInterface<String> aBag, boolean coorrectResult){
 		System.out.print("Testing isEmpty with ");
@@ -95,7 +98,7 @@ public class ResizeableArrayBagDemo {
 		System.out.println();
 	}
 	
-	//Tests the method getFrequency
+	/** Tests the method getFrequency*/
 	private static void testFrequency(BagInterface<String> aBag, String[] tests){
 		System.out.println("\nTesting the method getFrequencyOf:");
 		for(int index = 0; index < tests.length; index++){
@@ -104,7 +107,7 @@ public class ResizeableArrayBagDemo {
 		}
 	}
 	
-	//Tests the method contains
+	/** Tests the method contains*/
 	private static void testContains(BagInterface<String> aBag, String[] tests){
 		System.out.println("\nTesting the method contains");
 		for(int index = 0; index < tests.length; index++){
@@ -113,8 +116,7 @@ public class ResizeableArrayBagDemo {
 		}
 	}
 	
-	//Tests the method toArray while displaying the bag
-	//测试toArray方法且显示包中的内容
+	/** Tests the method toArray while displaying the bag*/
 	private static void displayBag(BagInterface<String> aBag){
 		System.out.println("The bag contains " + aBag.getCurrentSize() + 
 				" string(s), as follows:");
@@ -125,3 +127,72 @@ public class ResizeableArrayBagDemo {
 		System.out.println();
 	}//end displayBag
 }
+
+/*
+Expected output:
+
+Testing isEmpty with an empty bag:
+isEmpty finds the bag 
+empty: OK
+
+Adding to the bag more strings that its initial capacity
+Adding to the bag: A D B A C A D 
+The bag contains 7 string(s), as follows:
+A D B A C A D 
+Testing isEmpty with a bag that is not empty
+isEmpty finds the bag 
+not empty: OK.
+
+
+Testing the method getFrequencyOf:
+In this bag, the count of A is 3
+In this bag, the count of B is 1
+In this bag, the count of C is 1
+In this bag, the count of D is 2
+In this bag, the count of Z is 0
+
+Testing the method contains
+Does this bag contain A? true
+Does this bag contain B? true
+Does this bag contain C? true
+Does this bag contain D? true
+Does this bag contain Z? false
+
+Removing a string from the bag:
+remove() returnsD
+The bag contains 6 string(s), as follows:
+A D B A C A 
+
+Removing "B" from the bag:
+remove("B") returns true
+The bag contains 5 string(s), as follows:
+A D A A C 
+
+Removing "A" from the bag:
+remove("A") returns true
+The bag contains 4 string(s), as follows:
+C D A A 
+
+Removing "C" from the bag:
+remove("C") returns true
+The bag contains 3 string(s), as follows:
+A D A 
+
+Removing "Z" from the bag:
+remove("Z") returns false
+The bag contains 3 string(s), as follows:
+A D A 
+
+Remove the specific string in the bag
+The bag contains 1 string(s), as follows:
+D 
+
+Clearing the bag:
+Testing isEmpty with an empty bag:
+isEmpty finds the bag 
+empty: OK
+
+The bag contains 0 string(s), as follows:
+ 
+ 
+ */
