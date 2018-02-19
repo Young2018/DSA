753ea209eafb8b498a25b4e4255c395c431a88ed
young2018 <young6918@163.com>
2018-02-19 23:15:51 +0800

    添加了类的说明和语句的注释

--- src/cn/young22/dsa/ch05/StackDemo1.java
+++ src/cn/young22/dsa/ch05/StackDemo1.java
@@ -1,29 +1,43 @@
 package cn.young22.dsa.ch05;
 
 import cn.young22.dsa.ch06.LinkedStack;
-
+/** 测试LinkedStack方法*/
 public class StackDemo1 {
 	public static void main(String[] args){
-		//假设OurStack已经有了
+
+		// 初始化stringStacks
 		StackInterface<String> stringStack = new LinkedStack<>();
 		
+		// 想栈中添加几个字符串
 		stringStack.push("Jim");
 		stringStack.push("Jess");
 		stringStack.push("Jill");
 		stringStack.push("Jane");
 		stringStack.push("Joe");
 		
+		// 取栈顶元素的值
 		String top = stringStack.peek();		//returns "Joe"
 		System.out.println(top + " is at the top of the stack"); 
 		
+		// 删除栈顶元素的值
 		top = stringStack.pop();				//returns "Joe"
 		System.out.println(top + " is removed from the stack");
 		
+		// 取栈顶元素的值
 		top = stringStack.peek();				//returns "Jane"
 		System.out.println(top + " is at the top of the stack");
 		
+		// 删除栈顶元素的值
 		top = stringStack.pop();				//returns "Jane"
 		System.out.println(top + " is removed from the stack");
 		
 	}
 }
+/**
+Output:
+
+Joe is at the top of the stack
+Joe is removed from the stack
+Jane is at the top of the stack
+Jane is removed from the stack
+ */
