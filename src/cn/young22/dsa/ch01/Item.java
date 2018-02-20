0fc58be3ed59df664e45be530e18b93bf16e840f
young2018 <young6918@163.com>
2018-02-20 16:30:17 +0800

    修改了注释的格式

--- src/cn/young22/dsa/ch01/Item.java
+++ src/cn/young22/dsa/ch01/Item.java
@@ -4,8 +4,8 @@ import jdk.nashorn.internal.objects.annotations.Getter;
 
 /** 一个用来描述商品名称和价格的类*/
 public class Item {
-	private String description;	//物品描述
-	private int    price;		//物品价格，以分为单位
+	private String description;	// 物品描述
+	private int    price;		// 物品价格，以分为单位
 	
 	/** 构造器方法，
 	 * 将客户端传来的productDescription赋值给description
@@ -14,7 +14,7 @@ public class Item {
 	public Item(String productDescription, int productPrice){
 		description = productDescription;
 		price = productPrice;
-	}//end constructor
+	}// end constructor
 	
 	@Getter
 	public String getDescription() {
@@ -26,7 +26,7 @@ public class Item {
 		return price;
 	}
 	
-	//以下面的格式输出对象的信息
+	// 以下面的格式输出对象的信息
 	@Override
 	public String toString() {
 		return description + "\t$" + price / 100 + "." + price % 100;
