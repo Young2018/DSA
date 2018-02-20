42832c396e2999f5ebc38b1effd663c307cece8c
young2018 <young6918@163.com>
2018-02-20 16:30:40 +0800

    修改了注释的格式

--- src/cn/young22/dsa/ch01/OnlineShopper.java
+++ src/cn/young22/dsa/ch01/OnlineShopper.java
@@ -10,31 +10,31 @@ import cn.young22.dsa.ch02.ArrayBag1;
 public class OnlineShopper {
 
 	public static void main(String[] args) {
-		//模拟商品
+		// 模拟商品
 		Item[] items = {new Item("Bird feeder", 2050),
 				new Item("Squirrel guard", 1547),
 				new Item("Bird batch", 4499),
 				new Item("Sunflower seeds", 1295)
 		};
 		
-		//创建
+		// 创建
 		BagInterface<Item> shoppingCart = new ArrayBag1<>();
-		//物品的总价格
+		// 物品的总价格
 		int totalCost = 0;
 		
-		//将挑选的商品加到购物车中
+		// 将挑选的商品加到购物车中
 		for(int index = 0; index < items.length; index++){
 			Item nextItem = items[index];
 			shoppingCart.add(nextItem);
 			totalCost = totalCost + nextItem.getPrice();
 		}
 		
-		//每次删除一个购物车中的商品并显示商品信息
+		// 每次删除一个购物车中的商品并显示商品信息
 		while(!shoppingCart.isEmpty()){
 			System.out.println(shoppingCart.remove());
 		}
 		
-		//显示商品总价格
+		// 显示商品总价格
 		System.out.println("Total cost: " + "\t$" + totalCost / 100 + "." + totalCost % 100);
 		
 	}
