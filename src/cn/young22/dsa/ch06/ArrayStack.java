ee102e16d8f366578ef6644ffff8d6c6132832ad
young2018 <young6918@163.com>
2018-02-20 13:51:07 +0800

    增加了类的说明和一些方法的注释

--- src/cn/young22/dsa/ch06/ArrayStack.java
+++ src/cn/young22/dsa/ch06/ArrayStack.java
@@ -4,19 +4,24 @@ import java.util.Arrays;
 import java.util.EmptyStackException;
 
 import cn.young22.dsa.ch05.StackInterface;
-
+/** ArrayStack是用数组实现的栈
+ * 	它实现了StackInterface的功能
+ * */
 public class ArrayStack<T> implements StackInterface<T>{
-	private T[] stack;
-	private int topIndex;
-	private boolean initialized = false;
-	private static final int DEFAULT_CAPACITY = 50;
-	private static final int MAX_CAPACITY = 10000;
+	private T[] stack;						// 泛型的栈数组
+	private int topIndex;					// 记录栈顶位置的变量
+	private boolean initialized = false;	// 初始化标识符
+	private static final int DEFAULT_CAPACITY = 50;	// 默认栈容量为50
+	private static final int MAX_CAPACITY = 10000;	// 最大的栈容量为10000
 	
+	/** 默认构造器方法，生成一个默认容量的栈对象*/
 	public ArrayStack(){
 		this(DEFAULT_CAPACITY);
 	}//end default constructor
 	
+	/** 客户指定栈容量的构造方法，根据用户指定的栈容量生成一个栈对象*/
 	public ArrayStack(int initialCapacity) {
+		// 检查客户指定的栈容量是否是小于最大栈容量的
 		checkCapacity(initialCapacity);
 		
 		// The cast is safe because the new array contains null entries
@@ -29,14 +34,21 @@ public class ArrayStack<T> implements StackInterface<T>{
 
 // <Implementations of the stack operations go here.>
 	
+	/** push方法将客户指定的值加入到栈顶*/
 	@Override
 	public void push(T newEntry) {
+		
 		checkInitilization();
 		ensureCapacity();
 		stack[topIndex + 1] = newEntry;
 		topIndex++;
 	}
-
+	
+	/** pop方法删除栈顶元素
+	 *  先检查栈是否正常初始化，若未正常初始化，则抛出异常
+	 *  然后再检查栈是否为空，若栈空，则抛出异常
+	 *  若栈对象初始化成功且栈不为空，则删除栈顶元素
+	 * */
 	@Override
 	public T pop() {
 		checkInitilization();
@@ -49,7 +61,11 @@ public class ArrayStack<T> implements StackInterface<T>{
 			return top;
 		}
 	}
-
+	
+	/** 获取栈顶元素的值
+	 *  与pop方法类似，先要检查栈对象是否初始化成功和栈是否为空
+	 *  当栈初始化成功且不为空时，返回栈顶元素的值
+	 * */
 	@Override
 	public T peek() {
 		checkInitilization();
@@ -59,12 +75,14 @@ public class ArrayStack<T> implements StackInterface<T>{
 			return stack[topIndex];
 		}
 	}
-
+	
+	/** 查看栈是否为空*/
 	@Override
 	public boolean isEmpty() {
 		return topIndex < 0;
 	}
-
+	
+	/** 清除栈中的所有元素*/
 	@Override
 	public void clear() {
 		//当栈不为空时，则一直pop出栈顶元素
